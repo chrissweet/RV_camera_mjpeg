52c644ec5b8b67d692b3cc595e1546de67f86341
chrissweet <csweet1@nd.edu>
2020-12-09 20:56:19 -0500

    Initial Commit

---
+++ RV_Camera_mjpeg/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RV_Camera_mjpeg
+//
+//  Created by Christopher Sweet on 12/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
